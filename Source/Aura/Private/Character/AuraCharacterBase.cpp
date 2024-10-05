26d9634f8fec37e65e86636d78845762556f6f39
SecretPasta <boris.shoostin@gmail.com>
2024-10-05 22:21:59 +0300

    Added Weapon and created BPs

--- Source/Aura/Private/Character/AuraCharacterBase.cpp
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -2,12 +2,14 @@
 
 
 #include "Character/AuraCharacterBase.h"
-
+#include "Components/SkeletalMeshComponent.h"
 
 AAuraCharacterBase::AAuraCharacterBase()
 {
 	PrimaryActorTick.bCanEverTick = false;
-
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+	Weapon->SetupAttachment(GetMesh(),FName(TEXT("WeaponHandSocket")));
+	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 }
 
 void AAuraCharacterBase::BeginPlay()
