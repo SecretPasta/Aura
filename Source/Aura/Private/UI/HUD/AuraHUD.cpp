f0d3f3771dc0cc36cbaa5eafd59617bb2af3c775
SecretPasta <boris.shoostin@gmail.com>
2024-10-19 14:18:28 +0300

    Created HUD class

---
+++ Source/Aura/Private/UI/HUD/AuraHUD.cpp
@@ -0,0 +1,17 @@
+// Copyright Boris Shoostin
+
+
+#include "UI/HUD/AuraHUD.h"
+
+#include "UI/Widget/AuraUserWidget.h"
+
+void AAuraHUD::BeginPlay()
+{
+	Super::BeginPlay();
+	UUserWidget* Widget = CreateWidget<UUserWidget>(GetWorld(),OverlayWidgetClass);
+	if (Widget)
+	{
+		Widget->AddToViewport();
+	}
+	
+}
