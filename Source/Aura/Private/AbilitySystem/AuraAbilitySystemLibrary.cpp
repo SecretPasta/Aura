69fbfb14477e07aad122b8bbb314bba6defca543
SecretPasta <boris.shoostin@gmail.com>
2025-06-24 20:57:08 +0300

    Added AbilitySystemLibrary class

---
+++ Source/Aura/Private/AbilitySystem/AuraAbilitySystemLibrary.cpp
@@ -0,0 +1,32 @@
+// Copyright Boris Shoostin
+
+
+#include "AbilitySystem/AuraAbilitySystemLibrary.h"
+
+#include "Kismet/GameplayStatics.h"
+#include "Player/AuraPlayerState.h"
+#include "UI/HUD/AuraHUD.h"
+#include "UI/WidgetController/AuraWidgetController.h"
+
+UOverlayWidgetController* UAuraAbilitySystemLibrary::GetOverlayWidgetController(const UObject* WorldContextObject)
+{
+	APlayerController* PC = UGameplayStatics::GetPlayerController(WorldContextObject, 0);
+	if (!IsValid(PC)) return nullptr;
+	
+	AAuraHUD* AuraHUD = Cast<AAuraHUD>(PC->GetHUD());
+	if (!IsValid(AuraHUD)) return nullptr;
+	
+	AAuraPlayerState* PS = PC->GetPlayerState<AAuraPlayerState>();
+	if (!IsValid(PS)) return nullptr;
+	
+	UAbilitySystemComponent* ASC =  PS->GetAbilitySystemComponent();
+	if (!IsValid(ASC)) return nullptr;
+	
+	UAttributeSet* AS = PS->GetAttributeSet();
+	if (!IsValid(AS)) return nullptr;
+	
+	const FWidgetControllerParams WidgetControllerParams(PC,PS,ASC, AS);
+
+	return AuraHUD->GetOverlayWidgetController(WidgetControllerParams);
+	
+}
---
+++ Source/Aura/Public/AbilitySystem/AuraAbilitySystemLibrary.h
@@ -0,0 +1,23 @@
+// Copyright Boris Shoostin
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "AuraAbilitySystemLibrary.generated.h"
+
+class UOverlayWidgetController;
+/**
+ * Blueprint Ability System Library class
+ */
+UCLASS()
+class AURA_API UAuraAbilitySystemLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+public:
+
+	//Blueprint pure for Static functions, static functions also usually require a context object inorder to work properly
+	UFUNCTION(BlueprintPure, Category="AuraAbilitySystemLibrary|WidgetController")
+	static UOverlayWidgetController* GetOverlayWidgetController(const UObject* WorldContextObject);
+};
