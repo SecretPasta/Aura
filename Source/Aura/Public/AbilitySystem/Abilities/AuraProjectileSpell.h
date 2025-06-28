d9c1eb65f53a87540cbae43b91ab62dbed95b82f
SecretPasta <boris.shoostin@gmail.com>
2025-06-28 19:36:36 +0300

    Added Projectile Gameplay Ability

---
+++ Source/Aura/Public/AbilitySystem/Abilities/AuraProjectileSpell.h
@@ -0,0 +1,22 @@
+// Copyright Boris Shoostin
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AuraGameplayAbility.h"
+#include "AuraProjectileSpell.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAuraProjectileSpell : public UAuraGameplayAbility
+{
+	GENERATED_BODY()
+
+public:
+
+protected:
+
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+};
