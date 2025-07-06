ed42681d9165960f6dc42c4c21ee3b765ddf6b10
SecretPasta <boris.shoostin@gmail.com>
2025-07-06 18:21:00 +0300

    Added Character Class Data Asset

---
+++ Source/Aura/Private/AbilitySystem/Data/CharacterClassInfo.cpp
@@ -0,0 +1,9 @@
+// Copyright Boris Shoostin
+
+
+#include "AbilitySystem/Data/CharacterClassInfo.h"
+
+FCharacterClassDefaultInfo UCharacterClassInfo::GetClassDefaultInfo(ECharacterClass CharacterClass)
+{
+	return CharacterClassInformation.FindChecked(CharacterClass);
+}
