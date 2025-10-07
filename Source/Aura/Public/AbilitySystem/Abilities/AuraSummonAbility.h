c4806b352ac7e7c2c97acfcdaad378c0895532e4
SecretPasta <boris.shoostin@gmail.com>
2025-10-07 15:38:10 +0300

    Spawning minions now

--- Source/Aura/Public/AbilitySystem/Abilities/AuraSummonAbility.h
+++ Source/Aura/Public/AbilitySystem/Abilities/AuraSummonAbility.h
@@ -19,6 +19,9 @@ public:
 	UFUNCTION(BlueprintCallable)
 	TArray<FVector> GetSpawnLocations();
 
+	UFUNCTION(BlueprintPure, Category="Summoning")
+	TSubclassOf<APawn> GetRandomMinionClass();
+	
 	UPROPERTY(EditDefaultsOnly, Category = "Summoning")
 	int32 NumMinions = 5;
 	
@@ -34,4 +37,5 @@ public:
 	//How many Degrees between each minion around the spawner
 	UPROPERTY(EditDefaultsOnly, Category = "Summoning")
 	float SpawnSpread = 90.f;
+	
 };
