6e922c58a83b8a7803b03501814986d9e19a7362
SecretPasta <boris.shoostin@gmail.com>
2026-01-10 20:36:46 +0200

    Created a Passive Ability Class

---
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraPassiveAbility.cpp
@@ -0,0 +1,29 @@
+// Copyright Boris Shoostin
+
+
+#include "AbilitySystem/Abilities/AuraPassiveAbility.h"
+
+#include "AbilitySystemBlueprintLibrary.h"
+#include "AbilitySystem/AuraAbilitySystemComponent.h"
+
+class UAuraAbilitySystemComponent;
+
+void UAuraPassiveAbility::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
+                                          const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
+                                          const FGameplayEventData* TriggerEventData)
+{
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+	UAuraAbilitySystemComponent* AuraASC = Cast<UAuraAbilitySystemComponent>(UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(GetAvatarActorFromActorInfo()));
+	if (AuraASC && !AuraASC->DeactivatePassiveAbility.IsBoundToObject(this))
+	{
+		AuraASC->DeactivatePassiveAbility.AddUObject(this, &UAuraPassiveAbility::ReceiveDeactivate);
+	}
+}
+
+void UAuraPassiveAbility::ReceiveDeactivate(const FGameplayTag& AbilityTag)
+{
+	if (AbilityTags.HasTagExact(AbilityTag))
+	{
+		EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true, false);
+	}
+}
