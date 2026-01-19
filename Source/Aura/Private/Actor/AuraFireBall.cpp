b10ba6610e8b3cae3e18edae3da43f3bb1fc7071
SecretPasta <boris.shoostin@gmail.com>
2026-01-19 11:20:51 +0200

    Created FireBall Actor Class

---
+++ Source/Aura/Private/Actor/AuraFireBall.cpp
@@ -0,0 +1,28 @@
+// Copyright Boris Shoostin
+
+
+#include "Actor/AuraFireBall.h"
+
+#include "GameFramework/ProjectileMovementComponent.h"
+
+
+AAuraFireBall::AAuraFireBall()
+{
+	PrimaryActorTick.bCanEverTick = false;
+	ProjectileMovement->PrimaryComponentTick.bCanEverTick = false;
+	ProjectileMovement->SetAutoActivate(false);
+}
+
+void AAuraFireBall::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AAuraFireBall::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	
+}
+
+
--- Source/Aura/Public/Actor/AuraProjectile.h
+++ Source/Aura/Public/Actor/AuraProjectile.h
@@ -37,7 +37,7 @@ protected:
 	void OnHomingTargetDeath(AActor* DeadActor);
 
 	UFUNCTION()
-	void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+	virtual void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
 	TObjectPtr<USphereComponent> Sphere;
